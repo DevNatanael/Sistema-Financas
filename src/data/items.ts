a0531012f1c13acdd8866aad58f9858bfc46566e
DevNatanael <you@example.com>
2022-08-27 13:51:48 -0300

    primeiro commit

---
+++ src/data/items.ts
@@ -0,0 +1,34 @@
+import {Item} from '../types/Item'
+
+export const items: Item[] = [
+  {
+    date: new Date(2022, 9, 15),
+    category: "food",
+    title: "McDonalds",
+    value: 32.12,
+  },
+  {
+    date: new Date(2022, 9, 16),
+    category: "food",
+    title: "Burger King",
+    value: 29.12,
+  },
+  {
+    date: new Date(2022, 9, 17),
+    category: "rent",
+    title: "aluuguel apt",
+    value: 2300,
+  },
+  {
+    date: new Date(2022, 9, 18),
+    category: "food",
+    title: "Bobs",
+    value: 7.12,
+  },
+  {
+    date: new Date(2022, 9, 30),
+    category: "salary",
+    title: "Salário",
+    value: 2500,
+  },
+];
